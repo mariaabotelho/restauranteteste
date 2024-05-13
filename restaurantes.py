e539a6f8dd5bfcd5691537d193ecc56cde7bc9f7
mariaabotelho <168577465+mariaabotelho@users.noreply.github.com>
2024-05-13 10:33:48 -0300

    restaurantes.py

--- restaurantes.py
+++ restaurantes.py
@@ -9,10 +9,10 @@ df = pd.read_excel(caminho_do_arquivo)
 
 if 'categoria' not in df.columns:
     st.error("A coluna 'categoria' não foi encontrada. Verifique se o nome está correto.")
-    st.stop()  # Parar execução se a coluna não for encontrada
+    st.stop()  # se a coluna não for encontrada no meu df
 
 # pessoa vai selecionar uma categoria
-categoria_selecionada = st.selectbox("Escolha uma categoria:", df['categoria'].unique())
+categoria_selecionada = st.selectbox("escolhe uma categoria:", df['categoria'].unique())
 
 
 dados_filtrados = df[df['categoria'] == categoria_selecionada]
@@ -20,8 +20,8 @@ dados_filtrados = df[df['categoria'] == categoria_selecionada]
 # vai contar restaurantes por bairro na categoria que selecionar
 contagem_por_bairro = dados_filtrados['bairro'].value_counts()
 
-# Criando um gráfico de barras
-st.write(f"Gráfico mostrando a quantidade de restaurantes por bairro na categoria: {categoria_selecionada}")
+# vai criar um gráfico
+st.write(f"gráfico mostrando a quantidade de restaurantes por bairro na categoria: {categoria_selecionada}")
 plt.figure(figsize=(10, 5))
 plt.bar(contagem_por_bairro.index, contagem_por_bairro.values, color='blue')
 plt.xlabel('Bairro')
